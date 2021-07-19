3442db9da8ba369dc84ce58fc4396e85504368b4
mutmainnahrahim <78946873+mutmainnahrahim@users.noreply.github.com>
2021-07-19 21:25:26 +0800

    Update qurban.php

--- qurban.php
+++ qurban.php
@@ -41,24 +41,25 @@
             <div class="headline-container">
                 <h2>Qurban77 - 1439H/2018</h2>
                 <div class="slides-container">
-                    <img src="img/Artikel/Slide1.PNG" alt="Slide1">
-                    <img src="img/Artikel/Slide2.PNG" alt="Slide2">
-                    <img src="img/Artikel/Slide3.PNG" alt="Slide3">
-                    <img src="img/Artikel/Slide4.PNG" alt="Slide4">
-                    <img src="img/Artikel/Slide5.PNG" alt="Slide5">
-                    <img src="img/Artikel/Slide6.PNG" alt="Slide6">
-                    <img src="img/Artikel/Slide7.PNG" alt="Slide7">
-                    <img src="img/Artikel/Slide8.PNG" alt="Slide8">
-                    <img src="img/Artikel/Slide9.PNG" alt="Slide9">
-                    <img src="img/Artikel/Slide10.PNG" alt="Slide10">
-                    <img src="img/Artikel/Slide11.PNG" alt="Slide11">
-                    <img src="img/Artikel/Slide12.PNG" alt="Slide12">
-                    <img src="img/Artikel/Slide13.PNG" alt="Slide13">
-                    <img src="img/Artikel/Slide14.PNG" alt="Slide14">
-                    <img src="img/Artikel/Slide15.PNG" alt="Slide15">
-                    <img src="img/Artikel/Slide16.PNG" alt="Slide16">
-                    <img src="img/Artikel/Slide17.PNG" alt="Slide17">
-                    <img src="img/Artikel/Slide18.PNG" alt="Slide18">
+                    <img src="img/Artikel/Slide18.PNG" alt="Slide1">
+                    <img src="img/Artikel/Slide1.PNG" alt="Slide2">
+                    <img src="img/Artikel/Slide2.PNG" alt="Slide3">
+                    <img src="img/Artikel/Slide3.PNG" alt="Slide4">
+                    <img src="img/Artikel/Slide4.PNG" alt="Slide5">
+                    <img src="img/Artikel/Slide5.PNG" alt="Slide6">
+                    <img src="img/Artikel/Slide6.PNG" alt="Slide7">
+                    <img src="img/Artikel/Slide7.PNG" alt="Slide8">
+                    <img src="img/Artikel/Slide8.PNG" alt="Slide9">
+                    <img src="img/Artikel/Slide9.PNG" alt="Slide10">
+                    <img src="img/Artikel/Slide10.PNG" alt="Slide11">
+                    <img src="img/Artikel/Slide11.PNG" alt="Slide12">
+                    <img src="img/Artikel/Slide12.PNG" alt="Slide13">
+                    <img src="img/Artikel/Slide13.PNG" alt="Slide14">
+                    <img src="img/Artikel/Slide14.PNG" alt="Slide15">
+                    <img src="img/Artikel/Slide15.PNG" alt="Slide16">
+                    <img src="img/Artikel/Slide16.PNG" alt="Slide17">
+                    <img src="img/Artikel/Slide17.PNG" alt="Slide18">
+                    <img src="img/Artikel/Slide18.PNG" alt="Slide19">
                 </div>
             </div>
 
